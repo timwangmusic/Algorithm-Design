190396f5ed4675615daa69ef3a3dad998710f628
ForAlMighty <weihewang2012@gmail.com>
2017-10-31 23:09:27 -0700

    Min-heap Optimization
    Finally successfully added min-heap implementation by repeatedly pop out visited nodes. Tested using a 2-d network topology. The previous test case was 1-d.

--- PrimsAlgorithm.py
+++ PrimsAlgorithm.py
@@ -1,52 +1,50 @@
 from nodes import MSTNode
-from heapq import heapify, heappush, heappop
-from collections import defaultdict
-import sys
+from heapq import heappush, heappop
 
-class Prims():
+class Prims:
     """
     This class implement the Prim's algorithm to find the minimum spanning tree
     in an undirected weighted graph.
     """
-    def __init__(self, network):
+    def __init__(self, network = [MSTNode(data = (0, 0))]):
         """
         params:
             network: a list of Minimum-spanning tree nodes that are connected per graph topology
             n: network size
         """
         self.nodes = network
-        self.Q = network
+        self.networkSize = len(network)
+        self.heap = [(0, self.nodes[-1])]
         self.mst = []
-        self.cost = 0
+        self.mst_cost = 0
 
     def findMin(self):
-        candidate = None
-        cost = float('inf')
-        for node in self.Q:
-            if node.cost <= cost:
-                cost = node.cost
-                candidate = node
-        self.Q.remove(candidate)
+        cost, candidate = heappop(self.heap)
+        while self.heap and candidate.visited:
+            cost, candidate = heappop(self.heap)
         return candidate
 
     def constructMST(self):
-        while self.Q:
+        while len(self.mst) < self.networkSize - 1:
             node = self.findMin()
             node.visited = True
-            print (node.key)
             for p in node.neighbors:
-                if p.visited: continue
-                new_dist = node.getDist(p)
-                if new_dist < p.cost:
-                    p.cost = new_dist
-                    p.keyNode = node
+                if not p.visited:
+                    new_dist = node.getDist(p)
+                    if new_dist < p.cost:
+                        p.cost = new_dist
+                        p.keyNode = node
+                        heappush(self.heap, (p.cost, p))
             if node.keyNode:
                 self.mst.append((node, node.keyNode))
 
     def MSTCost(self):
+        """
+        Construct minimum-spanning tree and calculate cost.
+        """
         self.constructMST()
-        self.cost = sum([x[0].cost for x in self.mst])
-        return self.cost
+        self.mst_cost = sum([x[0].cost for x in self.mst])
+        return self.mst_cost
 
     def printMST(self):
         for edge in self.mst:
@@ -54,11 +52,11 @@ class Prims():
 
 class Test():
     def __init__(self):
-        s = MSTNode(key = 0, data = (0,0))
+        s = MSTNode(key = 0, data = (0,10))
         a = MSTNode(key = 1, data = (-3, 0))
-        b = MSTNode(key = 2, data = (-5,0))
+        b = MSTNode(key = 2, data = (-5,10))
         c = MSTNode(key = 3, data = (5,0))
-        d = MSTNode(key = 4, data = (20,0))
+        d = MSTNode(key = 4, data = (20,-5))
         s.addEdges([a, b, c, d])
         a.addEdges([s, b, d])
         b.addEdges([s, a])
