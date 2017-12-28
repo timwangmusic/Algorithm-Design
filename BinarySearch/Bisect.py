8e73afef97404b851958e339e6a2228077f768a4
ForAlMighty <weihewang2012@gmail.com>
2017-12-28 15:11:39 -0800

    Create Bisect.py

---
+++ BinarySearch/Bisect.py
@@ -0,0 +1,33 @@
+# bisect_left: Searching for first position such that all numbers to the left are less than target
+# bisect_right: Searching for first position such that all numbers to the left are less than or equals to target
+def bisect_left(nums, target, l, r):
+     while l <= r:
+         m = l + (r-l)//2
+         if nums[m] >= target:
+             r = m - 1
+         else:
+             l = m + 1
+     return l
+
+
+def bisect_right(nums, target, l, r):
+    while l <= r:
+        m = l + (r-l)//2
+        if nums[m] <= target:
+            l = m + 1
+        else:
+            r = m - 1
+    return r
+
+def searchRange(nums, target, l, r, left):
+    while l <= r:
+        m = l + (r-l)//2
+        if nums[m] > target or left and nums[m] == target:
+            r = m - 1
+        else:
+            l = m + 1
+    return l if left else r
+
+nums = [1, 2, 2, 2, 4, 5]
+print ("The left-most position for %d is %d" % (2, searchRange(nums, 2, 0, len(nums)-1, True)))
+print ("The right-most position for %d is %d" % (2, searchRange(nums, 2, 0, len(nums)-1, False)))
