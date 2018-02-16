b3cdf18849981d5a5d2a035e610280abd0ab99af
ForAlMighty <weihewang2012@gmail.com>
2018-02-15 18:16:35 -0800

    Update BinaryTreeToDoublyLinkedList.py

--- Trees/BinaryTreeToDoublyLinkedList.py
+++ Trees/BinaryTreeToDoublyLinkedList.py
@@ -1,40 +1,34 @@
-from BinaryNode import BinaryNode as Node
-
-def BT2DoublyLL(root):
-    node = root
-    stack = []
-    prev = None
-    head = None     # Doubly-linkedlist head
-    while stack or node:
-        if node:
-            stack.append(node)
-            node = node.left
-        else:
-            node = stack.pop()
-            if prev == None:
-                head = node
+# Definition for a Node.
+class Node(object):
+    def __init__(self, val, left, right):
+        self.val = val
+        self.left = left
+        self.right = right
+"""
+class Solution(object):
+    def treeToDoublyList(self, root):
+        """
+        :type root: Node
+        :rtype: Node
+        """
+        if root == None: return None
+        first = None
+        stack = []
+        cur = root
+        prev = None
+        while cur or stack:
+            if cur:
+                stack.append(cur)
+                cur = cur.left
             else:
-                prev.right = node
-                node.left = prev
-            prev = node
-            node = node.right
-    return head
-
-# Test case and print doubly-linked list
-root = Node(10)
-root.left = Node(12)
-root.right = Node(15)
-root.left.left = Node(25)
-root.left.right = Node(30)
-root.right.left = Node(36)
-
-def printDoublyLL(head):
-    while head.right != None:
-        print (head.key)
-        head = head.right
-
-    while head != None:
-        print (head.key)
-        head = head.left
-
-printDoublyLL(BT2DoublyLL(root))
+                cur = stack.pop()
+                if not first:
+                    first = cur
+                cur.left = prev
+                if prev: 
+                    prev.right = cur
+                prev = cur
+                cur = cur.right
+        first.left = prev
+        prev.right = first
+        return first
