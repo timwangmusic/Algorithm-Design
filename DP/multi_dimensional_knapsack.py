9e772037d10fc86bd723d78085c59c66c488b643
Tim Wang <weihewang2012@gmail.com>
2020-01-10 11:49:33 -0800

    naming adjustments

--- DP/multi_dimensional_knapsack.py
+++ DP/multi_dimensional_knapsack.py
@@ -25,10 +25,9 @@ def dp(time_allowance: int, budget: int, idx: int, places: List[Place], memo: di
                 prev_res_same_budget = dp(time_allowance, budget, idx - 1, places, memo)
                 prev_res = dp(time_allowance - place.time_needed, budget - place.cost, idx - 1, places, memo)
                 if prev_res.score + place.score >= prev_res_same_budget.score:
-                    new_result = Result()
+                    new_result = memo[time_allowance, budget, idx]
                     new_result.places = list(prev_res.places) + [place]  # copy places list
                     new_result.score = prev_res.score + place.score
-                    memo[time_allowance, budget, idx] = new_result
                 else:
                     memo[time_allowance, budget, idx] = dp(time_allowance, budget, idx - 1, places, memo)
             else:
@@ -46,6 +45,6 @@ if __name__ == "__main__":
     place_a = Place("A", 4.0, 1, 2)
     place_b = Place("B", 3.5, 2, 1)
     place_c = Place("C", 5.0, 2, 1)
-    res = multiDimensionalKnapsack(2, 1, [place_a, place_b, place_c])
-    pp(res.score)
-    pp([place.name for place in res.places])
+    res = multiDimensionalKnapsack(4, 2, [place_a, place_b, place_c])
+    pp(res.score)  # prints 8.5
+    pp([place.name for place in res.places])  # prints ['B', 'C']
